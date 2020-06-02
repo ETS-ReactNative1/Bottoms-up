692fba6547c8381688f24f4bcfa6770c5fa7c042
Bauter <bauter09@gmail.com>
2020-06-01 20:47:20 -0500

    verified server and app start on designated ports, breif start to API (depenencies)

---
+++ utils/API.js
@@ -0,0 +1,13 @@
+//===================================================
+// Import requires files
+//===================================================
+
+import axios from 'axios';
+import { /*New key HERE*/ } from 'react-native-dotenv';
+import React from 'react';
+
+// Define API functions inside exported module.
+
+export default {
+
+};
