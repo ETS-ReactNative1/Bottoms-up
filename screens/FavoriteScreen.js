cec1ac5823befdc699447a83a9322cc8c936e220
One Punch <hchang004@gmail.com>
2020-06-03 21:43:55 -0500

    added basic layout

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -10,60 +10,13 @@ import {
 } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { MonoText } from '../components/StyledText';
-import Popular from '../components/Popular';
-import Classic from '../components/Classic';
-import Martini from '../components/Martini';
-import Tropical from '../components/Tropical';
-import NonAlcoholic from '../components/NonAlcoholic';
+import Favorites from '../components/Favorite';
 
 export default function HomeScreen() {
   return (
     <ScrollView>
-      <Popular />
-      <Classic />
-      <Martini />
-      <Tropical />
-      <NonAlcoholic />
+      <Favorites />
     </ScrollView>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
   );
 }
 
