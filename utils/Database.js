bfedebd9153eee8772780bab29ea01af573dd32b
Bauter <bauter09@gmail.com>
2020-06-03 19:43:47 -0500

    created 'Database.js' for DB call functions to go into for better organization

---
+++ utils/Database.js
@@ -0,0 +1,14 @@
+//===================================================
+// Import requires files
+//===================================================
+
+import axios from 'axios';
+import React from 'react';
+
+// Define API functions inside exported module.
+
+export default {
+
+    // DATABASE backend calls go here
+
+}
