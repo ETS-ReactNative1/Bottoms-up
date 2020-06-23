481020c2d74f1fe1dc35f33643c6f6fc56dd66bd
Bauter <bauter09@gmail.com>
2020-06-22 21:11:12 -0500

    working opn share button fundementals, may need external packages

---
+++ components/ShareButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Share, View, Button, Alert } from 'react-native';
+
+
+export default ShareButton = (props) => {
+    let recipe = props.link
+    console.log(recipe)
+
+    const onShare = async () => {
+        try {
+            const result = await Share.share({
+                url: props.link,
+                message: "Check this out!",
+                title: "useless title"
+                // Needs work here, android has restrictions that iOS doesn't
+
+            });
+            if (SpeechRecognitionResultList.action === Share.sharedAction) {
+                if (SpeechRecognitionResultList.activityType) {
+                    console.log("shared with activity type of " + result.activityType)
+                } else {
+                    console.log(shared)
+                    Alert.alert("shared!")
+                }
+            } else if ( result.action === Share.dismissedAction) {
+                console.log(dismissedAction)
+            }
+            
+        } catch (error) {
+            Alert.alert(error.message)
+        }
+    };
+
+    return (
+        <View style={{ marginTop: 50}}>
+            <Button onPress={onShare} title="Share" />
+        </View>
+    
+    )
+}
